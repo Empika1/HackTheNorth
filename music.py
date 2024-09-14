0071db4549ac0f96af6f582504dcb7e0da06b4e4
Empika1 <dravidts1@gmail.com>
2024-09-14 11:12:50 -0400

    may  or may not be goond

--- music.py
+++ music.py
@@ -8,6 +8,82 @@ names = mido.get_output_names()
 print(names)
 
 out = mido.open_output(names[2])
-out.send(mido.Message('note_on', note=60, velocity=64, time=1))
-time.sleep(1)
-out.send(mido.Message('note_on', note=60, velocity=64, time=1))
+
+class Note:
+    def __init__(self, note, time, velocity, length): #length of 1 is a quarter note
+        self.note = note
+        self.time = time
+        self.velocity = velocity
+        self.length = length
+
+class Timeline:
+    def __init__(self, channel, notes = []):
+        self.channel = channel
+        self.notes = notes
+
+class Piece:
+    def __init__(self, bpms = [], timelines = []):
+        self.bpms = bpms
+        self.timelines = timelines
+
+startTime = time.time()
+def init():
+    global startTime
+    startTime = time.time()
+
+def noteTimeToTime(noteTime, bpms):
+    lastBpmNoteTime = 0
+    lastBpmTime = 0
+    lastBpm = -1
+    for bpm in bpms:
+        if noteTime >= bpm[1]:
+            lastBpmNoteTime = bpm[1]
+            if(lastBpm != -1):
+                lastBpmTime = bpm[1] * 60 / lastBpm
+            else:
+                lastBpmTime = 0
+            lastBpm = bpm[0]
+        else:
+            break
+    return (noteTime - lastBpmNoteTime) * 60 / lastBpm + lastBpmTime
+
+def playTimeline(timeline, bpms, timelineOnI, timelineOffI): #kind of a coroutine. yields back to playPiece
+    channel = timeline.channel
+    notes = timeline.notes
+
+    global startTime
+    currentTime = time.time()
+    if timelineOnI < len(notes):
+        currentOnNote = notes[timelineOnI]
+        if currentTime - startTime >= noteTimeToTime(currentOnNote.time, bpms):
+            out.send(mido.Message('note_on', note=currentOnNote.note, velocity=currentOnNote.velocity, channel=channel))
+            timelineOnI += 1
+    if timelineOffI < len(notes):
+        currentOffNote = notes[timelineOffI]
+        if currentTime - startTime >= noteTimeToTime(currentOffNote.time + currentOffNote.length, bpms):
+            out.send(mido.Message('note_off', note=currentOffNote.note, velocity=currentOffNote.velocity, channel=channel))
+            timelineOffI += 1
+    else:
+        return (timelineOnI, timelineOffI, True)
+    return (timelineOnI, timelineOffI, False)
+
+def playPiece(piece):
+    timelines = piece.timelines
+    bpms = piece.bpms
+
+    timelineOnIs = [0] * len(timelines)
+    timelineOffIs = [0] * len(timelines)
+    dones = [False] * len(timelines)
+    while True:
+        for i in range(len(timelines)):
+            timelineOnIs[i], timelineOffIs[i], dones[i] = playTimeline(timelines[i], bpms, timelineOnIs[i], timelineOffIs[i])
+        if all(dones):
+            break    
+
+
+timeline1 = Timeline(0)
+piece = Piece([], [timeline1])
+for i in range(32):
+    timeline1.notes.append(Note(60 + i, i, 64, 1))
+    piece.bpms.append((120 + 20 * i, i))
+playPiece(piece)
