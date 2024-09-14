891203446b72817bc9fc4b9a633e67200dddf5c5
Empika1 <dravidts1@gmail.com>
2024-09-14 09:54:15 -0400

    not cursed

--- music.py
+++ music.py
@@ -1,16 +1,13 @@
 import mido
-import pygame
+import time
 import pygame.midi
 
 mido.set_backend("mido.backends.portmidi")
 
 names = mido.get_output_names()
-print(names) 
-
+print(names)
 
 out = mido.open_output(names[2])
-out.send(mido.Message('note_on', note=60, velocity=64))
-
-#msg.copy(channel=1)
-#mido.Message('note_on', channel=1, note=60, velocity=64, time=0)
-#time.sleep(50)
+out.send(mido.Message('note_on', note=60, velocity=64, time=1))
+time.sleep(1)
+out.send(mido.Message('note_on', note=60, velocity=64, time=1))
